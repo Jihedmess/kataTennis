df30c1b1331792fa38090573f284515faf41caef
jihed.messaoud <jihed.messaoud@ext.carrefour.com>
2026-03-22 20:33:03 +0100

    init projet

---
+++ src/test/java/com/jihed/kata/tennis/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihed.kata.tennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
